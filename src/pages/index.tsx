4be0ab513192671e98be39047737d3337b4fa3e3
tajimulhasan <tajimulhasan1@gmail.com>
2024-10-28 02:52:37 +0600

    signup structure implement done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Signup from "./signup";
 
 export default function Home() {
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,51 +1,57 @@
-
-import { useForm } from "react-hook-form";
-type FormData = {
-  firstName: string;
-  lastName: string;
-};
+import { Button, Form } from "react-bootstrap";
+import styles from "./styles.module.scss";
+import classNames from "classnames";
+import Link from "next/link";
 
 const Signup = () => {
-  const {
-    register,
-    handleSubmit,
-
-    formState: { errors },
-  } = useForm<FormData>();
-
-  const onSubmit = (data: FormData) => {
-    alert(JSON.stringify(data));
-  };
-
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label>First Name</label>
-        <input
-          {...register("firstName", {
-            required: true,
-            maxLength: 20,
-            pattern: /^[A-Za-z]+$/i,
-          })}
-        />
-        {errors?.firstName?.type === "required" && (
-          <p>This field is required</p>
-        )}
-        {errors?.firstName?.type === "maxLength" && (
-          <p>First name cannot exceed 20 characters</p>
-        )}
-        {errors?.firstName?.type === "pattern" && (
-          <p>Alphabetical characters only</p>
-        )}
-        <label>Laste Name</label>
-        <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-        {errors?.lastName?.type === "pattern" && (
-          <p>Alphabetical characters only</p>
-        )}
-        <label>Age</label>
-        <input type="submit" />
-      </form>
-    </div>
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <div className={styles.welcomeText}>
+            <p>Welcome Back!</p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam
+              quo tempore voluptas maxime molestias, quia deserunt ut repellat
+              eveniet sapiente?
+            </p>
+          </div>
+          <div className={styles.inputContainer}>
+            <div className={styles.tag}>
+            <p>Sign Up</p>
+            <p></p>
+            </div>
+           <div className={styles.inputField}>
+           <Form>
+              <Form.Group
+                className="mb-1"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" placeholder="Enter your name" />
+              </Form.Group>
+              <Form.Group
+                className="mb-1"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter your email address" />
+              </Form.Group>
+              <Form.Group
+                className="mb-1"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>password</Form.Label>
+                <Form.Control type="password" placeholder="Enter your password" />
+              </Form.Group>
+              <Button className={classNames(styles.button, "rounded-pill")}>Sign Up</Button>
+            </Form>
+           </div>
+           <p className="mt-3 text-center mb-0">Already have an account? <Link href="/">Login</Link></p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
